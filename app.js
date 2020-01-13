9f900a135dde18f3640309af7a86343f6c27dc36
vinsondevin <vinsondevin@gmail.com>
2020-01-13 11:47:52 -0800

    merging chatroom with code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cors = require('cors');
+/*var logger = require('morgan');*/
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sqlite3  =  require('sqlite3').verbose();
@@ -14,9 +15,11 @@ const  router  =  express.Router();
 //var users = require('./routes/users');
 var generate_uid = require('./routes/generate_uid');
 var customer = require('./routes/customer');
+var chat = require('./routes/chat');
 
 var app = express();
 
+/*app.use(logger('dev'));*/
 app.use(cors());
 router.use(bodyParser.urlencoded({ extended:  false }));
 router.use(bodyParser.json());
@@ -136,11 +139,14 @@ process.on('unhandledRejection', function (reason, promise)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //app.use('/api/v1/users', users);
 app.use('/api/v1/customer', customer);
 app.use('/api/v1/generate_uid', generate_uid);
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -148,6 +154,18 @@ app.use(function(req, res, next) {
    next();
  });
 
+ app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+  });
 
+ app.use('/', chat);
+
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   res.render('index');
+ });
 
 module.exports = app;
